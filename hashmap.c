efab423081bad26a7c92012b7d9d700e29ae0fae
Your Name <you@example.com>
2026-04-19 15:27:57 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -134,6 +134,22 @@ Pair * searchMap(HashMap * map,  char * key) {
 // Recuerde actualizar la variable size.
 
 void eraseMap(HashMap * map,  char * key) {    
+    long pos = hash(key,map->capacity) ;
+
+    while (map->buckets[pos] != NULL)
+    {
+        if (strcmp(map->buckets[pos]->key, key) == 0)
+        {
+            map->buckets[pos]->key = NULL ;
+            map->size -- ;
+            return ;
+        }
+
+        pos = (pos+1) % map->capacity ;   
+    }
+
+    return ;
+    
 
 
 }
